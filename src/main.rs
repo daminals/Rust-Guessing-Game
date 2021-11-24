33bfdba10b1b7b21d75dd8ae3bdd9e01dc99c804
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-11-24 14:27:18 -0500

    rust number guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
-    let upperB = 1000;
-    let lowerB = 0;
+    let upperB: i32 = 1000;
+    let lowerB: i32 = 0;
+    let mut rng = rand::thread_rng();
     println!("Guess the number!");
     println!("I am thinking of a number between {} and {}. Please try to guess it", lowerB, upperB);
+    let correct_num = rng.gen_range(lowerB..upperB);
+    game(correct_num);
 }
-fn game() {
+fn game(correct_num: i32) -> bool {
     let mut guess = String::new();
     io::stdin()
     .read_line(&mut guess)
     .expect("Failed to read line");
-
-    let trimmed = guess.trim(); 
-    match trimmed.parse::<u32>() {
-        Ok(i) => {
-        
-
-        },
-        Err(..) => println!("Oh honey that was NOT an integer..."),
-    };
-
-
+    
+    let guess_int = guess.trim().parse::<i32>().unwrap();
+    if  guess_int == correct_num {
+        println!("Yes! The correct guess was {}! What an excellent guess!", guess_int);
+        return true;
+    } else {
+        if guess_int > correct_num {
+            println!("No you silly goose, {} is GREATER than my number lol",guess_int);
+            game(correct_num);
+        }
+        else {
+            if guess_int < correct_num {
+                println!("No you silly goose, {} is LOWER than my number lol",guess_int);
+                game(correct_num);
+            }
+        }
+    }
+    return false;
 }
