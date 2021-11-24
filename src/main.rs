ac63ee93253dc3d33cf83dd4d1d2a135c9de8ca3
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-11-24 17:41:46 -0500

    tracks guess #"s

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ fn main() {
     println!("Guess the number!");
     println!("I am thinking of a number between {} and {}. Please try to guess it", lowerB, upperB);
     let correct_num = rng.gen_range(lowerB..upperB);
-    game(correct_num);
+    game(correct_num, 0);
 }
-fn game(correct_num: i32) -> bool {
+fn game(correct_num: i32, mut guessN: i32) -> bool {
+    guessN += 1;
     let mut guess = String::new();
     io::stdin()
     .read_line(&mut guess)
@@ -18,19 +19,16 @@ fn game(correct_num: i32) -> bool {
     
     let guess_int = guess.trim().parse::<i32>().unwrap();
     if  guess_int == correct_num {
-        println!("Yes! The correct guess was {}! What an excellent guess!", guess_int);
+        println!("Yes! The correct guess was {}! You have won this game in only {} guesses!", guess_int, guessN);
         return true;
     } else {
         if guess_int > correct_num {
             println!("No you silly goose, {} is GREATER than my number lol",guess_int);
-            game(correct_num);
         }
         else {
-            if guess_int < correct_num {
-                println!("No you silly goose, {} is LOWER than my number lol",guess_int);
-                game(correct_num);
-            }
+            println!("No you silly goose, {} is LOWER than my number lol",guess_int);
         }
+        game(correct_num, guessN);
     }
     return false;
 }
